ffa5fe22b6a14df2278fba6318c499369f53f914
Samuel Ogwuche <samogwuche98@gmail.com>
2025-11-18 11:59:16 +0100

    create the testimonial section component

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="px-4 sm:px-12 lg:px-24 xl:px-40 py-6">
+      <div className="text-center">
+        <h2 className="font-bold text-3xl sm:text-4xl text-gray-700 dark:text-white">
+          What <span className="text-primary">Clients</span> Say!
+        </h2>
+        <p className="mt-5 text-gray-500 dark:text-gray-300 mx-auto text-md max-sm:w-5/6 sm:w-3/4 lg:w-1/3 ">
+          See how our digital marketing agency helped clients achieve their
+          goals
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
